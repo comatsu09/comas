90b82706e19683b21ea3cb745c1ebbc374bcef97
松元　陽香 <komatsumoto09@gmail.com>
2020-11-25 10:35:14 +0900

    5th commit

--- app.py
+++ app.py
@@ -3,12 +3,19 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/', methods=['GET'])
+def get():
+    return render_template('index.html', \
+    title = 'Form Sample(get)', \
+    message = '名前を入力して下さい。')
 
+@app.route('/', methods=['POST'])
 def post():
-    name = request.form.get('name')
+    name = request.form['name']
+    return render_template('index.html', \
+    title = 'Form Sample(post)', \
+    message = 'こんにちは、{}さん'.format(name))
+
 
 if __name__ == '__main__':
     app.run()
