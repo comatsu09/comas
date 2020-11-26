713824bbaca04fde4efe6d58e0c36cc00350db0c
松元　陽香 <komatsumoto09@gmail.com>
2020-11-26 15:41:28 +0900

    9th commit

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    usename = db.Column(db.string(80), unique=True)
-    email = db.Coulum(db.String(120), unique=True)
+    usename = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
 
     def __init__(self, username, email):
         self.usename = username
--- run.py
+++ run.py
@@ -3,11 +3,23 @@ from flask import request
 from flask import render_template
 from janome.tokenizer import Tokenizer
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
 
 app = Flask(__name__)
 t = Tokenizer()
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+engine = create_engine('sqlite:///test.sqlite')
+Base = declarative_base()
+
+class Text(Base):
+    __tablename__ = 'test'
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String,nullable=False)
 
 @app.route('/',  methods=["GET"])
 def get():
@@ -27,6 +39,16 @@ def janome():
     return render_template('index.html', \
     title = '文節分けをします(post)', \
     message = '結果： {}'.format(res))
+
+    Base.metadate.create_all(engine)
+    SessionMaker = sessionmaker(bind=engine)
+    session = SessionMaker()
+    text_res = Text(text = res)
+#    db.session.add(text_res)
+#    db.session.commit()
+    session.add(text_res)
+    session.commit()
+
 
 if __name__ == '__main__':
     app.run()
